bd8bdc1c89599c0d577c9a86966036057adb9d23
salvadoradrianG <salvadoradrian4356@gmail.com>
2026-09-18 04:19:59 +0000

    complete

--- pogi/lib/main.dart
+++ pogi/lib/main.dart
@@ -1,56 +1,75 @@
 import 'package:flutter/material.dart';
 
-void main() {
+void main(){
   runApp(const MyApp());
 }
-
 
 class MyApp extends StatelessWidget{
   const MyApp({super.key});
     @override
- Widget build(BuildContext context) {
-   return MaterialApp(
-     debugShowCheckedModeBanner: false,
-     home: Scaffold(
-       body: Center(
-         child: Column(
-           mainAxisAlignment: MainAxisAlignment.center,
-           crossAxisAlignment: CrossAxisAlignment.center,
-           children: [
-
-             const Icon(
-               Icons.account_circle,
-               size: 100,
-               color: Colors.blue,
-             ),
-             const SizedBox(height: 0),
-
-             const Text(
-               'Adrian G. Salvador',
-               style: TextStyle(
-                 fontSize: 20,
-                 fontWeight: FontWeight.bold,
-               ),
-             ),
-
-             const Text(
-               "Master's in Information Technology",
-               style: TextStyle(
-                 fontSize: 14,
-                 color: Colors.grey,
-               ),
-             ),
-             const Text(
-               'MCC BSIT!',
-               style: TextStyle(
-                 color: Colors.grey,
-               ),
-             ),
-           ],
-         ), 
-       ), 
-     ), 
-   ); 
- }
+      Widget build(BuildContext context){
+        return const MaterialApp(
+          debugShowCheckedModeBanner: false,
+          home: Scaffold(
+            body: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Icon(Icons.account_circle_rounded, color: Colors.deepPurple, size: 145.0,
+                  ),
+                  Text(
+                    'Adrian G. Salvador', style: TextStyle(fontSize: 21, color: Colors.black,),
+                  ),
+                  Text(
+                    'Bachelor of Information Technology', style: TextStyle(fontSize: 16, color: Colors.grey),
+                  ),
+                  SizedBox(height: 150),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children:[
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children:[
+                          Icon(Icons.home_outlined, color: Colors.deepPurple, size: 45.0,
+                          ),
+                          Text(
+                            'Home', style: TextStyle(fontSize: 21, color: Colors.grey),
+                          )
+                        ]
+                      ),
+                      SizedBox(width: 100),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children:[
+                          Icon(Icons.menu_book, color: Colors.deepPurple, size: 45.0,
+                          ),
+                          Text(
+                            'Courses', style: TextStyle(fontSize: 21, color: Colors.grey),
+                          )
+                        ]
+                      ),
+                      SizedBox(width: 100),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children:[
+                          Icon(Icons.person_outlined, color: Colors.deepPurple, size: 45.0,
+                          ),
+                          Text(
+                            'Profile', style: TextStyle(fontSize: 21, color: Colors.grey),
+                          )
+                        ]
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 200),
+                  Text(
+                    'Ang Pogi mo Rodney!', style: TextStyle(fontSize: 16, color: Colors.grey),
+                  ),
+                ]
+              )
+            ),
+          ),
+        );
+      }
 }
- 
